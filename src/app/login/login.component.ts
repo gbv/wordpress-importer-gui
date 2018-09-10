f1a8686c8e8d1f0e7ba2afc7b1dc325857464d04
Sebastian Hofmann <hofmann.s@uni-jena.de>
2018-09-10 16:55:03 +0200

    improved login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 import {ConfigService, ImporterConfiguration} from "../config.service";
 import {HttpClient} from "@angular/common/http";
 import {Color, MessageService} from "../message.service";
@@ -18,6 +18,9 @@ export class LoginComponent implements OnInit {
   }
 
   private static LOGIN_PATH = "api/v1/auth/login";
+  private static TOKEN_VALIDITY_TIME = 1024 * 60 * 30;
+  private static KEY_TOKEN = "token";
+  private static KEY_TOKEN_EXPIRE = "tokenExpire";
 
   private login: FormGroup;
   private userName: FormControl;
@@ -35,6 +38,23 @@ export class LoginComponent implements OnInit {
       password : this.password
     });
 
+    if ("localStorage" in window) {
+      const storage = window.localStorage;
+
+      const key = storage.getItem(LoginComponent.KEY_TOKEN + "_" + this.configId);
+      const keyExpire = storage.getItem(LoginComponent.KEY_TOKEN_EXPIRE + "_" + this.configId);
+
+      if (key != null && keyExpire != null) {
+        let dateNumber: number;
+        if (dateNumber = parseInt(keyExpire)) {
+          const keyAge = (new Date()).valueOf() - dateNumber;
+          if (keyAge < LoginComponent.TOKEN_VALIDITY_TIME) {
+            this.setAuthToken(key, LoginComponent.TOKEN_VALIDITY_TIME - keyAge);
+          }
+        }
+      }
+    }
+
   }
 
   async getToken() {
@@ -45,7 +65,12 @@ export class LoginComponent implements OnInit {
       const options = {headers : {"Authorization" : `Basic ${btoa(this.userName.value + ":" + this.password.value)}`}};
       const authResponse = await this.http.get<AuthResponse>(repository + LoginComponent.LOGIN_PATH, options).toPromise();
       if(authResponse.login_success){
-        this.auth.token = authResponse.token_type + " " + authResponse.access_token;
+        const token = authResponse.token_type + " " + authResponse.access_token;
+        this.setAuthToken(token, LoginComponent.TOKEN_VALIDITY_TIME);
+        if ("localStorage" in window) {
+          window.localStorage.setItem(LoginComponent.KEY_TOKEN + "_" + this.configId, token);
+          window.localStorage.setItem(LoginComponent.KEY_TOKEN_EXPIRE + "_" + this.configId, new Date().valueOf().toString(10));
+        }
       } else {
         this.messageService.push({
           title: "Login Fehlgeschlagen!",
@@ -63,6 +88,10 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  private setAuthToken(token: string, expires: number) {
+    this.auth.token = token;
+    window.setTimeout(() => this.auth.token = null, expires)
+  }
 }
 
 interface AuthResponse {
